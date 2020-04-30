1f6931e9238c531bfa37ccebc0e734bfe492fc02
o-su <9551674+o-su@users.noreply.github.com>
2020-04-30 15:14:42 +0200

    fixed interface-name-prefix, explicit-function-return-type rules

--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,14 @@
+// no-console
+console.log("test");
+
+// interface-name-prefix
+export interface Test {
+  name: string;
+}
+
+// explicit-function-return-type
+[].map(() => undefined); // arrow function without return type
+
 export function functionWithoutReturnType() {
   console.log("test");
 }
